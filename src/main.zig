f200d733d74076e294fd3eb6c3332b67952c7074
tuftman-0 <28765011+tuftman-0@users.noreply.github.com>
2026-02-15 09:50:35 -0500

    mostly experimentation

--- src/main.zig
+++ src/main.zig
@@ -1,16 +1,26 @@
-const std = @import("std");
 const regexcomp = @import("regexcomp");
+const std = @import("std");
 const ast = @import("regex_ast.zig");
 const nfa = @import("nfa.zig");
 const dfa = @import("dfa.zig");
 
 
 pub fn main() !void {
-    // 1. Setup the allocator
+    // var stdout_buffer: [1024]u8 = undefined;
+    // var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
+    // const stdout = &stdout_writer.interface;
+    // _ = stdout;
+
     const allocator = std.heap.page_allocator;
 
-    const input = "(1|10)(1|10)*";
+    // const input = "(1|10)(1|10)*";
+    // const input = "a*b*a*b*";
     // const input = "11(11)*|111(111)*";
+    const input = "(10|01)*";
+    // const input = "a+(bab)*(0|11)+|(1|010|~)*11(~|a)";
+    // const input = "(a(ab)*b)*";
+    // const input = "(ab|ba)*";
+    // const input = "(h|e|l|o)*hello";
 
     std.debug.print("Input: {s}\n", .{input});
 
@@ -30,8 +40,8 @@ pub fn main() !void {
     var automaton = nfa.NFA{};
     const frag = try nfa.compileNode(arena.allocator(), tree, &automaton);
     automaton.states.items[frag.accept].is_accept = true;
-    std.debug.print("\nNFA\n", .{});
-    nfa.dumpNFA(&automaton);
+    // std.debug.print("\nNFA\n", .{});
+    // nfa.dumpNFA(&automaton);
 
 
     const sparse_dfa = try dfa.makeDFA(
@@ -39,13 +49,18 @@ pub fn main() !void {
         &automaton,
         frag.start,
     );
-    // defer sparse_dfa.deinit(allocator);
+    // defer sparse_dfa.deinit(allocator); // not needed because we use arena
 
-    std.debug.print("\nsparse DFA\n", .{});
-    dfa.dumpDFA(&sparse_dfa);
+    // std.debug.print("\nsparse DFA\n", .{});
+    // dfa.dumpDFA(&sparse_dfa);
 
     const dense_dfa = try dfa.toDense(arena.allocator(), &sparse_dfa);
-    std.debug.print("\ndense DFA\n", .{});
-    dfa.dumpDense(&dense_dfa);
+    // std.debug.print("\ndense DFA\n", .{});
+    // dfa.dumpDense(&dense_dfa);
     try dfa.dumpParker(&dense_dfa);
+    const matched = dfa.matches(&dense_dfa, "01010");
+
+    const output: []const u8 = if (matched) "accepted" else "rejected";
+    std.debug.print("\n{s} blehg\n" , .{output});
+
 }
