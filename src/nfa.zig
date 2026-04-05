57a3c6dac64b0f399ac4baf25413f7f9070a20ac
tuftman-0 <28765011+tuftman-0@users.noreply.github.com>
2026-04-05 10:21:44 -0400

    testing different StateID sizes

--- src/nfa.zig
+++ src/nfa.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 const ast = @import("regex_ast.zig");
 
-pub const StateId = u16; // indices for states in NFA and DFA (determines max number of states)
+pub const StateId = usize; // indices for states in NFA and DFA (determines max number of states)
 
 pub const Transition = union(enum) {
     ch: struct { ch: u8, to: StateId },
