da4e3b5828beaa36331b622c5bb8c54bb44043fb
tuftman-0 <28765011+tuftman-0@users.noreply.github.com>
2026-07-08 10:56:51 -0400

    started an online compilation experiment

---
+++ src/fastdfa.zig
@@ -0,0 +1,9 @@
+const std = @import("std");
+const nfa_mod = @import("nfa.zig");
+
+const NFA = nfa_mod.NFA;
+const StateId = nfa_mod.StateId;
+const BitSet = std.bit_set.DynamicBitSetUnmanaged;
+
+
+
